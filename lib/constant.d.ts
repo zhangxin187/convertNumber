e7e68fde432e943668e8129af57e7db7055f11e4
zhangxin <862257810@qq.com.com>
2022-09-19 00:49:53 +0800

    remove lib

--- lib/constant.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const chnNumChar: string[];
-export declare const chnUnitChar: string[];
-export declare const specialUnit: string[];
-export declare const ZERO = "\u96F6";
--- lib/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const convertToChineseNumber: (num: number | string) => string;
